cdda7562b4b00773908016c48a27c3c584d1e3f4
LovelyXinf <146376679+LovelyXinf@users.noreply.github.com>
2023-10-12 16:58:39 -0700

    a

--- src/Exceptions/CouldNotSendNotification.php
+++ src/Exceptions/CouldNotSendNotification.php
@@ -2,3 +2,10 @@
 
 namespace NotificationChannels\WhatsApp\Exceptions;
 
+final class CouldNotSendNotification extends \Exception
+{
+    public static function serviceRespondedWithAnError($responseBody)
+    {
+        return new self($responseBody);
+    }
+}
